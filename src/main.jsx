7ab4591e14548a9b9d90f4f7e58601ae1e6a2f6d
someonemightseethis <ahmedrameez.rajput@gmail.com>
2023-11-10 03:23:28 +0500

    what?

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import './index.css'
 // import Login from './components/Login.jsx'
 // import Registration from './components/Registration.jsx'
 // import BusinessRegistrationForm from './components/BusinessRegistrationForm.jsx'
-import Dashboard from './components/Dashboard.jsx'
-// import BusinessInfo from './components/BusinessInfo.jsx'
+// import Dashboard from './components/Dashboard.jsx'
+import BusinessInfo from './components/BusinessInfo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Login /> */}
     {/* <Registration /> */}
     {/* <BusinessRegistrationForm /> */}
-    <Dashboard />
-    {/* <BusinessInfo /> */}
+    {/* <Dashboard /> */}
+    <BusinessInfo />
   </React.StrictMode>,
 )
