f01e2808390d8821516bacbab8cffe2a9764cdbb
someonemightseethis <ahmedrameez.rajput@gmail.com>
2023-11-17 20:39:30 +0500

    Add taos.js library and parallax effect, update package name and dependencies, and add animations using tailwindcss-animated and taos plugins

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ function Home() {
 							d="M0,224L48,213.3C96,203,192,181,288,154.7C384,128,480,96,576,90.7C672,85,768,107,864,128C960,149,1056,171,1152,181.3C1248,192,1344,192,1392,192L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
 						></path>
 					</svg>
-					<h1 className="text-white text-8xl text-center pb-28 font-semibold">
+					<h1 className="text-white text-8xl text-center pb-28 font-semibold delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0" data-taos-offset="400">
 						This is Parallax Effect
 					</h1>
 					<div className="grid xl:lg:grid-cols-2 md:sm:xs:grid-cols-1 gap-16 px-24">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 // Import the tw-elements plugin using ES module syntax
 import twElementsPlugin from "tw-elements/dist/plugin.cjs";
+import tailwindcssAnimated from 'tailwindcss-animated';
+import taosScrollAnimations from 'taos/plugin';
 /** @type {import('tailwindcss').Config} */
 
 export default {
@@ -8,9 +10,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
+  transform: {
+    content: (content) => content.replace(/taos:/g, ''),
+  },
   theme: {
     extend: {},
   },
-  plugins: [twElementsPlugin],
+  plugins: [twElementsPlugin, tailwindcssAnimated, taosScrollAnimations],
   darkMode: "class",
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
 };
